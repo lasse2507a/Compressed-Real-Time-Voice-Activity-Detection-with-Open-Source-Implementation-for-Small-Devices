a3d48767453c448cb18d047087fc4bd0b3ae6e3e
Lasse Andersen <lran18@student.aau.dk>
2023-03-16 12:46:22 +0100

    finished the record audio function

--- src/__main__.py
+++ src/__main__.py
@@ -6,12 +6,18 @@ from audio_collection import record_audio
 F_SAMPLING = 16000
 RECORDING_SIZE = 1000
 recording_queue = Queue()
+stop = False
+
 
 if __name__ == '__main__':
     thread_record_audio = Thread(target=record_audio,
+                                 daemon=True,
                                  args=(RECORDING_SIZE,
                                        F_SAMPLING,
-                                       recording_queue))
+                                       recording_queue,
+                                       stop))
 
     thread_record_audio.start()
-    
+
+    input()
+    stop = True
