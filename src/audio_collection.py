fee2918fc6dbf524329b571cffb31857a3230d5a
Lasse Andersen <lran18@student.aau.dk>
2023-03-16 11:17:04 +0100

    grammar

--- src/audio_collection.py
+++ src/audio_collection.py
@@ -2,7 +2,7 @@ import sounddevice as sd
 import numpy as np
 
 def record_audio(recording_size, sampling_frequency, recording_queue):
-    print("Audio recordning started")
+    print("Audio recording started")
     recording_queue.put(sd.rec(recording_size,
                        samplerate = sampling_frequency,
                        channels = 1,
