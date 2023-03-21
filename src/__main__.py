aca8bc10fb475459412ca1566e07d092f83aa637
Lasse Andersen <lran18@student.aau.dk>
2023-03-21 10:37:45 +0100

    using queue for recordnings

--- src/__main__.py
+++ src/__main__.py
@@ -7,17 +7,17 @@ BLOCKSIZE = 200
 F_SAMPLING = 16000
 
 def main():
-    recordings = []
+    recordings = Queue()
     thread_stop_event = threading.Event()
     thread_record_audio = threading.Thread(target=record_audio, daemon=True, args=(F_SAMPLING, BLOCKSIZE, recordings, thread_stop_event))
 
     thread_record_audio.start()
 
-    time.sleep(10)
+    time.sleep(5)
     thread_stop_event.set()
     thread_record_audio.join()
-    print(len(recordings))
-    print(len(recordings[0][0]))
+
+    print(recordings.qsize())
 
 if __name__ == '__main__':
     main()
