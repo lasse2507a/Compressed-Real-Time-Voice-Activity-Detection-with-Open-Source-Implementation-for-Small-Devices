a68a760eefe155dcdadaea7fa86b654c49e9818d
Lasse Andersen <lran18@student.aau.dk>
2023-03-28 09:41:46 +0200

    added unfinished function for mfsc preprocessing. Changed scipy to librosa

---
+++ src/preprocessing_mfsc.py
@@ -0,0 +1,7 @@
+import librosa
+
+def preprocessing_mfsc(samplerate, blocksize, recordings, melspecs, thread_stop_event):
+    print("MFSC preprocessing started")
+    while not thread_stop_event.is_set():
+        melspecs.put(librosa.feature.melspectrogram(y=recordings.get(), sr=samplerate, n_fft=blocksize))
+    print("MFSC preprocessing stopped")
