28a5d985f59ae52e8cdfa706df95172b3d7e1d88
Lasse Andersen <lran18@student.aau.dk>
2023-04-04 09:59:56 +0200

    made preprocessing oop

--- src/__main__.py
+++ src/__main__.py
@@ -1,40 +1,49 @@
 import threading
 import time
-
-import librosa
 import matplotlib.pyplot as plt
 import numpy as np
-
 from sine_wave_generator import SineWaveGenerator
 from audio_recorder import AudioRecorder
-#from preprocessing_mfsc import preprocessing_mfsc
+from preprocessing_mfsc import MFSCPreprocessor
 
 F_SAMPLING = 48000
 BLOCKSIZE = 128
 
 def real_time_implementation():
-    audio_recorder = AudioRecorder(F_SAMPLING, BLOCKSIZE)
-    thread_audio_recorder = threading.Thread(target=audio_recorder.start_recording, daemon=True)
-    thread_audio_recorder.start()
+    recorder = AudioRecorder(F_SAMPLING, BLOCKSIZE)
+    preprocessor = MFSCPreprocessor(F_SAMPLING, BLOCKSIZE)
+
+    thread_recorder = threading.Thread(target=recorder.start_recording, daemon=True)
+    thread_preprocessor = threading.Thread(target=preprocessor.start_preprocessing, args=(recorder.recordings,), daemon=True)
+
+    thread_recorder.start()
+    thread_preprocessor.start()
 
     time.sleep(1)
 
-    audio_recorder.stop_recording()
-    thread_audio_recorder.join()
+    recorder.stop_recording()
+    preprocessor.stop_preprocessing()
+    thread_recorder.join()
+    thread_preprocessor.join()
+
+    #plt.figure(figsize=(10, 4))
+    #librosa.display.specshow(librosa.power_to_db(melspecs.get(), ref=np.max), sr=F_SAMPLING, x_axis='time', y_axis='mel')
+    #plt.title('Mel spectrogram')
+    #plt.show()
 
 def frequency_test():
     t = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).time()
     f = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).frequency()
     signal = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).five_sine_wave()
-    signal_fft = np.fft.fft(signal)
+    signal_fft = np.abs(np.fft.fft(signal))[:int(BLOCKSIZE/2)]
 
     plt.figure(figsize=(16,9))
     plt.subplot(2, 1, 1)
-    plt.plot(t, signal)
+    plt.plot(t, signal, ".-")
     plt.subplot(2, 1, 2)
-    plt.plot(f, abs(signal_fft)[:int(BLOCKSIZE/2)])
+    plt.plot(f, signal_fft, ".-")
     plt.show()
 
 if __name__ == '__main__':
-    #frequency_test()
-    real_time_implementation()
+    frequency_test()
+    #real_time_implementation()
--- src/preprocessing_mfsc.py
+++ src/preprocessing_mfsc.py
@@ -1,33 +1,55 @@
 from queue import Queue
-
+import threading
 import librosa
 import numpy as np
 
-def preprocessing_mfsc(samplerate, blocksize, recordings, melspecs, thread_stop_event):
-    print("MFSC preprocessing started")
-    previous_half = Queue()
-    first_round = True
-    while not thread_stop_event.is_set():
-        recording = recordings.get()
-        recording_copy = np.copy(recording)
-        previous_half.put(recording_copy[int(blocksize/2):])
-        if not first_round:
-            signal = np.concatenate((previous_half.get(), recording))
-            melspecs.put(librosa.feature.melspectrogram(y=signal,
-                                                        sr=samplerate,
-                                                        n_fft=int(blocksize*1.5),
-                                                        hop_length=int(blocksize*1.5/2),
-                                                        window='hann',
-                                                        center=True,
-                                                        power=2.0,
-                                                        n_mels=64,
-                                                        fmin=300,
-                                                        fmax=8000))
-        else:
-            first_round = False
-    print("MFSC preprocessing stopped")
+class MFSCPreprocessor:
+    def __init__(self, samplerate, blocksize):
+        self.samplerate = samplerate
+        self.blocksize = blocksize
+        self.first_round = True
+        self.previous_half = Queue()
+        self.thread_stop_event = threading.Event()
+        self.melspecs = Queue()
+        self.melspecs.maxsize = 64
 
-    #plt.figure(figsize=(10, 4))
-    #librosa.display.specshow(librosa.power_to_db(melspecs.get(), ref=np.max), sr=F_SAMPLING, x_axis='time', y_axis='mel')
-    #plt.title('Mel spectrogram')
-    #plt.show()
+    def start_preprocessing(self, recordings):
+        print("MFSC preprocessing started")
+        while not self.thread_stop_event.is_set():
+            recording = recordings.get()
+            recording_copy = np.copy(recording)
+            self.previous_half.put(recording_copy[int(self.blocksize/2):])
+            if not self.first_round:
+                overlapped_recording = np.concatenate((self.previous_half.get(), recording))
+                self.melspecs.put(librosa.feature.melspectrogram(y=overlapped_recording,
+                                                    sr=self.samplerate,
+                                                    n_fft=int(self.blocksize*1.5),
+                                                    hop_length=int(self.blocksize*1.5/2),
+                                                    window='hann',
+                                                    center=True,
+                                                    power=2.0,
+                                                    n_mels=64,
+                                                    fmin=300,
+                                                    fmax=8000))
+            self.first_round = False
+            if self.melspecs.full():
+                print("MFSC preprocessing queue full")
+
+    def stop_preprocessing(self):
+        self.thread_stop_event.set()
+        print("MFSC preprocessing stopped")
+
+'''
+if not self.first_round:
+                overlapped_recording = np.concatenate((self.previous_half.get(), recording))
+                self.melspecs.put(librosa.feature.melspectrogram(y=overlapped_recording,
+                                                    sr=self.samplerate,
+                                                    n_fft=int(self.blocksize*1.5),
+                                                    hop_length=int(self.blocksize*1.5/2),
+                                                    window='hann',
+                                                    center=True,
+                                                    power=2.0,
+                                                    n_mels=64,
+                                                    fmin=300,
+                                                    fmax=8000))
+'''
