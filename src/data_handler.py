7c714668ef3db662e87ce2ef8d414e9f3ca71e72
Lasse Andersen <lran18@student.aau.dk>
2023-04-12 16:15:41 +0200

    almost finished data_handler

--- src/data_handler.py
+++ src/data_handler.py
@@ -9,9 +9,11 @@ class DataHandler:
         self.start_times = []
         self.end_times = []
         self.labels = []
-        self.indices = []
+        self.indices_correct_size = []
+        self.indices_current_file = []
+        self.is_same_file = False
 
-    def load_csv(self, path = 'data\\data_test.csv'):
+    def load_csv(self, path = 'data\\ava_speech_labels_v1.csv'):
         with open(path, encoding = "utf-8") as csvfile:
             csv_reader = csv.reader(csvfile, delimiter = ',')
             for row in csv_reader:
@@ -21,23 +23,29 @@ class DataHandler:
                 self.labels.append(int(row[3]))
 
     def create_data(self, size = 48000):
-        for i in self.start_times:
-            if (self.end_times[i] - self.start_times[i]) * self.samplerate > size:
-                self.indices.append(i)
+        for k in range(len(self.start_times)):
+            if (self.end_times[k] - self.start_times[k]) * self.samplerate >= size:
+                self.indices_correct_size.append(k)
 
-        os.makedirs("data\\training_data")
-        os.makedirs("data\\test_data")
+        os.makedirs("data\\training_data", exist_ok = True)
+        os.makedirs("data\\test_data", exist_ok = True)
 
         for file in os.listdir('data\\audio'):
-            current_file = open(file, 'r', encoding = "utf-8")
-            for index in self.indices:
-                if current_file.name == self.names[index]:
-                    begin = self.start_times[index] * self.samplerate
-                    end = self.start_times[index] * self.samplerate + size
-                    if index % 2 == 0:
-                        wavfile.write(f"data\\training_data\\{index}_{current_file.name}_training", self.samplerate, current_file[begin:end])
+            with open(file, 'r', encoding = "utf-8") as current_file:
+                for i in self.indices_correct_size:
+                    if self.names[i] == current_file.name:
+                        self.indices_current_file.append(self.indices_correct_size[i])
+                        self.is_same_file = True
+                    elif self.is_same_file:
+                        break
+                for j in self.indices_current_file:
+                    begin = self.start_times[j] * self.samplerate
+                    end = self.start_times[j] * self.samplerate + size
+                    if j % 2 == 0:
+                        wavfile.write(f"data\\training_data\\{j}_{current_file.name}_training", self.samplerate, current_file[begin:end])
                     else:
-                        wavfile.write(f"data\\test_data\\{index}_{current_file.name}_test", self.samplerate, current_file[begin:end])
+                        wavfile.write(f"data\\test_data\\{j}_{current_file.name}_test", self.samplerate, current_file[begin:end])
 
 datahandler = DataHandler(16000)
 datahandler.load_csv()
+datahandler.create_data()
