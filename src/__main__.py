f62ac66a2e1798b3d9e5af6e846f1ca084023236
Lasse Andersen <lran18@student.aau.dk>
2023-04-15 17:22:53 +0200

    restructured the code. changed range(len()) to enumerate in create_data

--- src/__main__.py
+++ src/__main__.py
@@ -3,13 +3,27 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as sp
-from data_handler import DataHandler
 from sine_wave_generator import SineWaveGenerator
-from audio_recorder import AudioRecorder
-from real_time_mfsc_preprocessing import RealTimeMFSCPreprocessor
+from data_handler import DataHandler
+from real_time_implementation.audio_recorder import AudioRecorder
+from real_time_implementation.real_time_mfsc_preprocessing import RealTimeMFSCPreprocessor
 
 F_SAMPLING = 48000
 BLOCKSIZE = 512
+
+def frequency_test():
+    t = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).time()
+    f = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).frequency()
+    signal = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).five_sine_wave(freq4 = 8500)
+    signal = np.convolve(signal, sp.firwin(numtaps = 32+1, cutoff = [300, 8000], window = 'hamming', pass_zero = False, fs = F_SAMPLING), mode = "same")
+    signal_fft = np.abs(np.fft.fft(signal))[:int(BLOCKSIZE/2)]
+
+    plt.figure(figsize=(16,9))
+    plt.subplot(2, 1, 1)
+    plt.plot(t, signal, ".-")
+    plt.subplot(2, 1, 2)
+    plt.plot(f, signal_fft, ".-")
+    plt.show()
 
 def data_generation():
     datahandler = DataHandler(44100)
@@ -39,21 +53,10 @@ def real_time_implementation():
     plt.plot(f, signal, ".-")
     plt.show()
 
-def frequency_test():
-    t = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).time()
-    f = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).frequency()
-    signal = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).five_sine_wave(freq4 = 8500)
-    signal = np.convolve(signal, sp.firwin(numtaps = 32+1, cutoff = [300, 8000], window = 'hamming', pass_zero = False, fs = F_SAMPLING), mode = "same")
-    signal_fft = np.abs(np.fft.fft(signal))[:int(BLOCKSIZE/2)]
-
-    plt.figure(figsize=(16,9))
-    plt.subplot(2, 1, 1)
-    plt.plot(t, signal, ".-")
-    plt.subplot(2, 1, 2)
-    plt.plot(f, signal_fft, ".-")
-    plt.show()
+def batch_implementation():
+    print("batch implementation")
 
 if __name__ == '__main__':
-    data_generation()
     #frequency_test()
-    #real_time_implementation()
+    #data_generation()
+    real_time_implementation()
---
+++ src/data/data_handler.py
@@ -0,0 +1,64 @@
+import csv
+import os
+import numpy as np
+import scipy.io.wavfile as wavfile
+
+class DataHandler:
+    def __init__(self, samplerate):
+        self.samplerate = samplerate
+        self.names = []
+        self.start_times = []
+        self.end_times = []
+        self.labels = []
+        self.indices_correct_size = []
+        self.indices_current_file = []
+        self.is_same_file = False
+
+    def load_csv(self, path = 'data\\ava_speech_labels_v1.csv'):
+        with open(path, encoding = "utf-8") as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter = ',')
+            for row in csv_reader:
+                self.names.append(row[0])
+                self.start_times.append(float(row[1]))
+                self.end_times.append(float(row[2]))
+                self.labels.append(int(row[3]))
+            print(len(self.names))
+            print(len(self.start_times))
+            print(len(self.end_times))
+            print(len(self.labels))
+
+    def create_data(self, size = 44100*3):
+        for k, start_time in enumerate(self.start_times):
+            if (self.end_times[k] - start_time) * self.samplerate >= size:
+                self.indices_correct_size.append(k)
+        print(len(self.indices_correct_size))
+
+        os.makedirs(f"data\\output\\training_data_{size}", exist_ok = True)
+        os.makedirs(f"data\\output\\test_data_{size}", exist_ok = True)
+
+        for file in os.listdir('data\\input'):
+            current_file = wavfile.read(os.path.join('data\\input', file))[1]
+            l = 0
+            for i in self.indices_correct_size:
+                if 'data\\input\\' + self.names[i] + '.wav' == os.path.join('data\\input', file):
+                    self.indices_current_file.append(i)
+                #    self.is_same_file = True
+                #elif self.is_same_file:
+                #    self.is_same_file = False
+                #    break
+            for j in self.indices_current_file:
+                begin = self.start_times[j] * self.samplerate
+                end = self.start_times[j] * self.samplerate + size
+                audio_data_portion = current_file[int(begin):int(end)]
+                l = l + 1
+                if l % 2 == 0:
+                    wavfile.write(f"data\\output\\training_data_{size}\\{j+1}_{self.names[j]}_training_{size}_{self.labels[j]}.wav",
+                                  self.samplerate, np.array(audio_data_portion, dtype=np.int16))
+                else:
+                    wavfile.write(f"data\\output\\test_data_{size}\\{j+1}_{self.names[j]}_test_{size}_{self.labels[j]}.wav",
+                                  self.samplerate, np.array(audio_data_portion, dtype=np.int16))
+
+if __name__ == '__main__':
+    datahandler = DataHandler(44100)
+    datahandler.load_csv()
+    datahandler.create_data()
