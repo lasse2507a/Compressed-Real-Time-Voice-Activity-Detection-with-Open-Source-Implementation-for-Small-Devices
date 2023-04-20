0a15612a90a9f4fbec0b3b115192f30dd955a4fe
Lasse Andersen <lran18@student.aau.dk>
2023-04-20 14:20:15 +0200

    started preprocessing for training data

--- src/__main__.py
+++ src/__main__.py
@@ -9,14 +9,14 @@ from real_time_implementation.audio_recorder import AudioRecorder
 from real_time_implementation.real_time_preprocessing import RealTimeMFSCPreprocessor
 
 F_SAMPLING = 48000
-BLOCKSIZE = 512
+SIZE = 512
 
 def frequency_test():
-    t = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).time()
-    f = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).frequency()
-    signal = SineWaveGenerator(BLOCKSIZE, F_SAMPLING).five_sine_wave(freq4 = 8500)
+    t = SineWaveGenerator(SIZE, F_SAMPLING).time()
+    f = SineWaveGenerator(SIZE, F_SAMPLING).frequency()
+    signal = SineWaveGenerator(SIZE, F_SAMPLING).five_sine_wave(freq4 = 8500)
     signal = np.convolve(signal, sp.firwin(numtaps = 32+1, cutoff = [300, 8000], window = 'hamming', pass_zero = False, fs = F_SAMPLING), mode = "same")
-    signal_fft = np.abs(np.fft.fft(signal))[:int(BLOCKSIZE/2)]
+    signal_fft = np.abs(np.fft.fft(signal))[:int(SIZE/2)]
 
     plt.figure(figsize=(16,9))
     plt.subplot(2, 1, 1)
@@ -31,8 +31,8 @@ def data_generation():
     datahandler.create_data(size = 44100*3, new_samplerate = 16000)
 
 def real_time_implementation():
-    recorder = AudioRecorder(F_SAMPLING, BLOCKSIZE)
-    preprocessor = RealTimeMFSCPreprocessor(F_SAMPLING, BLOCKSIZE)
+    recorder = AudioRecorder(F_SAMPLING, SIZE)
+    preprocessor = RealTimeMFSCPreprocessor(F_SAMPLING, SIZE)
 
     thread_recorder = threading.Thread(target=recorder.start_recording, daemon=True)
     thread_preprocessor = threading.Thread(target=preprocessor.start_preprocessing, args=(recorder.recordings,), daemon=True)
@@ -47,7 +47,7 @@ def real_time_implementation():
     thread_recorder.join()
     thread_preprocessor.join()
 
-    f = SineWaveGenerator(int(BLOCKSIZE), F_SAMPLING).frequency()
+    f = SineWaveGenerator(int(SIZE), F_SAMPLING).frequency()
     signal = preprocessor.melspecs.get()
     plt.figure(figsize=(16,9))
     plt.plot(f, signal, ".-")
--- src/batch_implementation/preprocessing.py
+++ src/batch_implementation/preprocessing.py
@@ -1,2 +1,11 @@
+import os
+import scipy.io.wavfile as wavfile
+
 class Preprocessor:
-    def __init__(self):
+    def __init__(self, samplerate, size):
+        self.samplerate = samplerate
+        self.size = size
+
+    def process(self, data_path):
+        for file in os.listdir(data_path):
+            current_file = wavfile.read(os.path.join(data_path, file))[1]
