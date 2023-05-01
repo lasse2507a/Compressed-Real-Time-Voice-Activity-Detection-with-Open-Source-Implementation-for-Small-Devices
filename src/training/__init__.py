de40ffcf6c640badaf71b1ba570cbb33e34aeab9
Lasse Andersen <lran18@student.aau.dk>
2023-05-01 17:16:01 +0200

    updated the data generator and the training code to use the data generator

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -22,9 +22,9 @@ def execute_training(training_data_path, validation_data_path):
     model.fit(x=training_data,
               validation_data=validation_data,
               epochs=12,
-              steps_per_epoch=len(training_data) // 25000,
               verbose=2,
-              callbacks=tf.keras.callbacks.ModelCheckpoint('models\\model_1'))
+              callbacks=tf.keras.callbacks.ModelCheckpoint('models\\model_1'),
+              use_multiprocessing=True)
 
 def visualize_model():
     model = CNNModel(K=40, L=20, M=10, N=100, classes=2, div=10, batch_size=25000, keep_prob=0.75, learning_rate=np.hstack((1e-3*np.ones(6),
--- src/training/data_generator.py
+++ src/training/data_generator.py
@@ -10,7 +10,7 @@ class DataGenerator(tf.keras.utils.Sequence):
         self.num_files = len(self.files)
 
     def __len__(self):
-        return int(np.ceil(self.num_files / float(self.batch_size)))
+        return int(np.floor(self.num_files / float(self.batch_size)))
 
     def __getitem__(self, idx):
         batch_files = self.files[idx * self.batch_size:(idx + 1) * self.batch_size]
@@ -18,8 +18,8 @@ class DataGenerator(tf.keras.utils.Sequence):
         batch_labels = []
         for file in batch_files:
             file_data = np.load(file)
-            label = int(os.path.basename(file).split("_")[-2])
             batch_data.append(np.reshape(file_data, (1, 40, 40)))
+            label = int(os.path.basename(file).split("_")[-2])
             batch_labels.append(label)
         batch_data = np.concatenate(batch_data, axis=0)
         batch_labels = np.array(batch_labels)
