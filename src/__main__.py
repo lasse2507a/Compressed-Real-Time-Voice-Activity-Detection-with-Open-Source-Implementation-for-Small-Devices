e5082f885d4e47523846554b32cb2055c2c01c32
Lasse Andersen <lran18@student.aau.dk>
2023-05-01 23:12:11 +0200

    Added function for generation MFSC example images

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,10 @@
 #from data_processing.__init__ import frequency_test
 #from data_processing.__init__ import data_generation
 #from data_processing.__init__ import preprocessing
+#from data_processing.__init__ import plot_mfsc
 #from real_time_implementation.__init__ import real_time_implementation
 #from training.__init__ import visualize_model
-from training.__init__ import execute_training
+#from training.__init__ import execute_training
 
 if __name__ == '__main__':
     #data_generation()
@@ -11,5 +12,6 @@ if __name__ == '__main__':
     #preprocessing()
     #real_time_implementation()
     #visualize_model()
-    execute_training(training_data_path='data\\output\\training_clip_len_17200samples\\mfsc_window_400samples',
-                     validation_data_path='data\\output\\test_clip_len_17200samples\\mfsc_window_400samples')
+    #execute_training(training_data_path='data\\output\\training_clip_len_17200samples\\mfsc_window_400samples',
+    #                 validation_data_path='data\\output\\test_clip_len_17200samples\\mfsc_window_400samples')
+    #plot_mfsc()
