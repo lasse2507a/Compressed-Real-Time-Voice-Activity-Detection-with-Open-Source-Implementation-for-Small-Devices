9bc11416d89fda7ef5c61db1d3e4b1baff54e366
lasse2507a <lran18@student.aau.dk>
2023-05-04 00:58:28 +0200

    added function for calculating confusion_matrix

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,3 +1,17 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from evaluation.data_loader import DataLoader
+from evaluation.confusion_matrix import confusion_matrix
+
 
 def evalution():
-    print("evaluation")
+    data_loader = DataLoader("data/output/prediction_audio_clip_2")
+    data, labels = data_loader.load_data_parallel()
+
+    model = tf.keras.models.load_model("models/cnn_model_original_25(12,8,5).h5")
+
+    predictions = model.predict(x=data, verbose=1)
+
+    cm = confusion_matrix(labels, predictions)
+    print(cm)
