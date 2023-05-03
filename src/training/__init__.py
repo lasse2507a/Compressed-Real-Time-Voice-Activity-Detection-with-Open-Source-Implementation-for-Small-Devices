286305c055bc2ec2ba0bc77e03810590b3a2c8e1
lasse2507a <lran18@student.aau.dk>
2023-05-03 22:06:51 +0200

    Updated architecture of cnn model v2

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -8,9 +8,9 @@ def execute_training():
     validation_data_path='data/output/validation_clip_len_17200samples/mfsc_window_400samples'
     model_name = 'cnn_model_v2'
     batch_size = 256
-    epochs = 40
+    epochs = 25
 
-    model = cnn_model_v2()
+    model = cnn_model_original()
     model.summary()
     model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
                   loss=tf.keras.losses.BinaryCrossentropy(),
@@ -20,9 +20,9 @@ def execute_training():
     validation_data = Generator(validation_data_path, batch_size)
 
     def scheduler(epoch, lr):
-        if epoch == 20:
+        if epoch == 12:
             return lr * 0.1
-        elif epoch == 30:
+        elif epoch == 20:
             return lr * 0.1
         else:
             return lr
