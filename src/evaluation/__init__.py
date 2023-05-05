0b42dc41237aef50168b484121c59c25eb69c0f0
lasse2507a <lran18@student.aau.dk>
2023-05-05 10:44:33 +0200

    moved evaluation to init in evaluation from main

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -10,6 +10,46 @@ from evaluation.data_loader import DataLoader
 from evaluation.confusion_matrix import confusion_matrix
 from evaluation.metrics import *
 
+def execute_evaluation():
+    # y, y_ = evaluation.predictions_webrtc()
+    # import matplotlib.pyplot as plt
+    # plt.plot(y_[0], label='Mode 0')
+    # plt.plot(y_[1], label='Mode 1')
+    # plt.plot(y_[2], label='Mode 2')
+    # plt.plot(y_[3], label='Mode 3')
+    # plt.xlim(-0.1, 30000)
+    # plt.ylim(-0.1, 1.1)
+    # plt.legend()
+    # plt.show()
+
+    y1, y1_ = predictions()
+
+    y2 = np.load('data/output/prediction_audio_clip_2/silero_labels.npy')
+    y2_ = np.load('data/output/prediction_audio_clip_2/silero_preds.npy')
+
+    fpr1, tpr1 = auc_roc(y1, y1_)
+    fpr2, tpr2 = auc_roc(y2, y2_)
+
+    plt.plot(fpr1, tpr1)
+    plt.plot(fpr2, tpr2)
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    auc_value1 = roc_auc_score(y1, y1_)
+    auc_value2 = roc_auc_score(y2, y2_)
+    plt.title(f"ROC Curve (AUC = {auc_value1:.4f}, AUC = {auc_value2:.4f})")
+    plt.show()
+
+    # recalls1, precisions1 = precision_recall_plot(y1, y1_)
+    # recalls2, precisions2 = precision_recall_plot(y2, y2_)
+
+    # plt.plot(recalls1, precisions1)
+    # plt.plot(recalls2, precisions2)
+    # #plt.xlim(0, 1.05)
+    # #plt.ylim(0, 1.05)
+    # plt.xlabel("Recall")
+    # plt.ylabel("Precision")
+    # plt.show()
+
 
 def predictions():
     data_loader_mfsc = DataLoader("data/output/prediction_audio_clip_2/mfsc_400samples")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score
+
 #import real_time_implementation.__init__ as real_time_implementation
 #import training.__init__ as training
 import evaluation.__init__ as evaluation
@@ -8,42 +6,4 @@ import evaluation.__init__ as evaluation
 if __name__ == '__main__':
     #real_time_implementation.real_time_implementation()
     #training.execute_training()
-
-    # y, y_ = evaluation.predictions_webrtc()
-    # import matplotlib.pyplot as plt
-    # plt.plot(y_[0], label='Mode 0')
-    # plt.plot(y_[1], label='Mode 1')
-    # plt.plot(y_[2], label='Mode 2')
-    # plt.plot(y_[3], label='Mode 3')
-    # plt.xlim(-0.1, 30000)
-    # plt.ylim(-0.1, 1.1)
-    # plt.legend()
-    # plt.show()
-
-    y1, y1_ = evaluation.predictions()
-
-    y2 = np.load('data/output/prediction_audio_clip_2/silero_labels.npy')
-    y2_ = np.load('data/output/prediction_audio_clip_2/silero_preds.npy')
-
-    fpr1, tpr1 = evaluation.auc_roc(y1, y1_)
-    fpr2, tpr2 = evaluation.auc_roc(y2, y2_)
-
-    plt.plot(fpr1, tpr1)
-    plt.plot(fpr2, tpr2)
-    plt.xlabel("FPR")
-    plt.ylabel("TPR")
-    auc_value1 = roc_auc_score(y1, y1_)
-    auc_value2 = roc_auc_score(y2, y2_)
-    plt.title(f"ROC Curve (AUC = {auc_value1:.4f}, AUC = {auc_value2:.4f})")
-    plt.show()
-
-    # recalls1, precisions1 = evaluation.precision_recall_plot(y1, y1_)
-    # recalls2, precisions2 = evaluation.precision_recall_plot(y2, y2_)
-
-    # plt.plot(recalls1, precisions1)
-    # plt.plot(recalls2, precisions2)
-    # #plt.xlim(0, 1.05)
-    # #plt.ylim(0, 1.05)
-    # plt.xlabel("Recall")
-    # plt.ylabel("Precision")
-    # plt.show()
+    evaluation.execute_evaluation()
