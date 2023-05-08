d57171f9ce0b464c5863cabf1dfce744a43d96b5
lasse2507a <lran18@student.aau.dk>
2023-05-08 12:28:16 +0200

    created class for real time inference lite

--- src/real_time_implementation/real_time_inference_lite.py
+++ src/real_time_implementation/real_time_inference_lite.py
@@ -1,25 +1,29 @@
+import threading
+from queue import Queue
 import numpy as np
 import tensorflow as tf
-from tflite_runtime.interpreter import Interpreter
 
-# Load the TFLite model into memory
-interpreter = Interpreter('my_model.tflite')
-interpreter.allocate_tensors()
 
-# Get the input and output tensors
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+class RealTimeInferenceLite:
+    def __init__(self, model_name):
+        self.thread_stop_event = threading.Event()
+        self.preds = Queue()
+        self.model = tf.keras.models.load_model(f'models/{model_name}')
+        self.interpreter = tf.lite.Interpreter(model_path=f"models/{model_name}")
+        self.input_details = self.interpreter.get_input_details()[0]
+        self.output_details = self.interpreter.get_output_details()[0]
 
-# Prepare input data
-input_data = np.random.rand(1, 40, 40, 1).astype(np.float32)
 
-# Set input tensor
-interpreter.set_tensor(input_details[0]['index'], input_data)
+    def start_inference(self, images_MFSC):
+        while not self.thread_stop_event.is_set():
+            image = images_MFSC.get()
+            self.interpreter.set_tensor(self.input_details['index'], image.astype(np.float32))
+            image = np.expand_dims(np.expand_dims(image, axis=-1), axis=0)
+            self.interpreter.allocate_tensors()
+            self.interpreter.invoke()
+            prediction = self.interpreter.get_tensor(self.output_details['index']).ravel()
+            self.preds.put(prediction)
 
-# Run inference
-interpreter.invoke()
 
-# Get output tensor
-output_data = interpreter.get_tensor(output_details[0]['index'])
-
-# Use output data for further processing or analysis
+    def stop_inference(self):
+        self.thread_stop_event.set()
