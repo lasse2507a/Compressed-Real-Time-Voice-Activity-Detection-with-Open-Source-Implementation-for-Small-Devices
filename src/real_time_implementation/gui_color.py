cd011eb94e46bfb4643adcf31d15748b152e1ebc
lasse2507a <lran18@student.aau.dk>
2023-05-08 14:42:24 +0200

    added plot to gui

--- src/real_time_implementation/gui_color.py
+++ src/real_time_implementation/gui_color.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-
+from queue import Queue
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class GUI(tk.Tk):
     def __init__(self, threshold):
@@ -9,6 +11,21 @@ class GUI(tk.Tk):
         self.color_label = tk.Label(self, text="Prediction color", font=("Helvetica", 20))
         self.color_label.pack(pady=50)
         self.threshold =  threshold
+        self.data = Queue(100)
+
+        # Create the figure and canvas objects
+        self.fig = plt.figure(figsize=(4, 4))
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        # Create a subplot for the plot
+        self.ax = self.fig.add_subplot(111)
+        self.ax.set_xlabel('Time')
+        self.ax.set_ylabel('Value')
+
+        # Create the color label
+        self.color_label = tk.Label(self, text="Prediction color", font=("Helvetica", 20))
+        self.color_label.pack(pady=20)
 
 
     def update_color(self, preds):
@@ -17,4 +34,12 @@ class GUI(tk.Tk):
             self.color_label.config(bg="green", text="Positive")
         else:
             self.color_label.config(bg="red", text="Negative")
+
+        self.data.get(pred)
+        if self.data.full():
+            self.data.get()
+        self.ax.clear()
+        self.ax.plot(self.data)
+        self.canvas.draw()
+
         self.update()
