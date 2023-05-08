1f1167eaab2651f62ecc108f9cf774125163c21f
Lasse Andersen <lran18@student.aau.dk>
2023-05-08 13:52:09 +0200

    gui update

--- src/real_time_implementation/gui_color.py
+++ src/real_time_implementation/gui_color.py
@@ -2,21 +2,21 @@ import tkinter as tk
 
 
 class GUI(tk.Tk):
-    def __init__(self, queue):
+    def __init__(self, preds):
         super().__init__()
         self.title("Binary Classification Prediction")
         self.geometry("300x300")
         self.color_label = tk.Label(self, text="Prediction color", font=("Helvetica", 20))
         self.color_label.pack(pady=50)
-        self.queue = queue
+        self.preds = preds
         self.update_color()
 
 
     def update_color(self):
-        while not self.queue.empty():
-            prediction = self.queue.get()
+        while not self.preds.empty():
+            prediction = self.preds.get()
             if prediction == 0:
                 self.color_label.config(bg="red", text="Negative")
             else:
                 self.color_label.config(bg="green", text="Positive")
-        self.after(100, self.update_color)
+        self.after(10, self.update_color)
