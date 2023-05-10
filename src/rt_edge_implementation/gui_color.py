569c42bd38a1138361b26141d1b431ed9363bdb5
Lasse Andersen <lran18@student.aau.dk>
2023-05-10 16:38:00 +0200

    changed size of gui color for edge

--- src/rt_edge_implementation/gui_color.py
+++ src/rt_edge_implementation/gui_color.py
@@ -5,7 +5,7 @@ class GUIColor(tk.Tk):
     def __init__(self, threshold):
         super().__init__()
         self.title("Binary Classification Prediction")
-        self.geometry("960x540")
+        self.geometry("400x200")
         self.color_label = tk.Label(self, text="Prediction color", font=("Helvetica", 20))
         self.color_label.pack(pady=50)
         self.threshold =  threshold
