2b3b7f4b588c8849b323073df444733a00cc8a4e
Lasse Andersen <lran18@student.aau.dk>
2023-05-11 11:54:06 +0200

    change save path of model visualization

--- src/training/model_visualization.py
+++ src/training/model_visualization.py
@@ -4,10 +4,8 @@ import visualkeras
 
 def visualize_model(model_name):
     model = tf.keras.models.load_model(f'models/{model_name}')
-    visualkeras.layered_view(model,
-                            legend=True,
-                            to_file='model.png').show() # Fix scaling
-
+    visualkeras.layered_view(model, legend=True, to_file=f'images\\{model_name.split(".")[0]}.png').show() # Fix scaling
+    
 
 if __name__ == '__main__':
     visualize_model('cnn_model_v4_25(12,8,5).h5')
